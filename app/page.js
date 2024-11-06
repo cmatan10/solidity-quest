8256b0de2f7e5694718413d69a3e3b2c89ab6848
cmatan10 <cmatan10@gmail.com>
2024-11-06 14:30:07 +0200

    deepkLink

--- app/page.js
+++ app/page.js
@@ -1,35 +1,26 @@
-import { Web3Modal } from '@web3modal/react';
-import { WagmiProvider } from 'wagmi';
 import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, WhatsNew } from '../sections';
-import { wagmiClient, ethereumClient } from '../web3/web3ModalConfig';
-import { Web3Provider } from '../web3/Web3provider';
 
 const Page = () => (
-  <WagmiProvider client={wagmiClient}>
-    <Web3Provider>
-      <div className="bg-primary-black overflow-hidden">
-        <Navbar />
-        <div className="relative">
-          <About />
-          <div className="gradient-03 z-0" />
-          <Explore />
-        </div>
-        <div className="relative">
-          <GetStarted />
-          <div className="gradient-04 z-0" />
-          <WhatsNew />
-        </div>
-        <div className="relative">
+  <div className="bg-primary-black overflow-hidden">
+    <Navbar />
+    <div className="relative">
+      <About />
+      <div className="gradient-03 z-0" />
+      <Explore />
+    </div>
+    <div className="relative">
+      <GetStarted />
+      <div className="gradient-04 z-0" />
+      <WhatsNew />
+    </div>
+    <div className="relative">
 
-          <div className="gradient-04 z-0" />
-          <Feedback />
-        </div>
-        <Footer />
-      </div>
-    </Web3Provider>
-    <Web3Modal projectId="9ea74fc0-0f80-4bc4-b98f-d6d4e256d34f" ethereumClient={ethereumClient} />
-  </WagmiProvider>
+      <div className="gradient-04 z-0" />
+      <Feedback />
+    </div>
+    <Footer />
+  </div>
 );
 
 export default Page;
--- web3/Web3provider.jsx
+++ web3/Web3provider.jsx
@@ -4,7 +4,7 @@
 /* eslint-disable no-use-before-define */
 import { createContext, useState, useEffect } from 'react';
 import Web3 from 'web3';
-import { Web3Modal } from '@web3modal/react';
+import WalletConnectProvider from '@walletconnect/web3-provider';
 import gameABI from '../interfaces/GameFactory.json';
 import nftABI from '../interfaces/NFTbadge';
 
@@ -18,26 +18,25 @@ export const Web3Provider = ({ children }) => {
   const [web3, setWeb3] = useState(null);
   const [tokenIDs, setTokenIDs] = useState([]);
 
-  const contractAddresses = {
-    1440002: {
-      gameAddress: '0xa7169fDC282c990b510E2d6428bBC0ED1F7f1EfE',
-      nftAddress: '0x1319a2fff9189496b5420256a0b19bBD36aed6eB',
-    },
-    80002: {
-      gameAddress: '0x41Ba2b107DF51e61519b7b975a25bC4DF7eD5D06',
-      nftAddress: '0xf55754207c6d8Ed08fD0C5be3D00a4B581718A6a',
-    },
-    11155111: {
-      gameAddress: '0x0f5841D53526aBdF15ef77c790aCd7839D86D9d0',
-      nftAddress: '0x1D5916eF2922d6783aF201A25af034d5024fA2f0',
-    },
-  };
-
   useEffect(() => {
     if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
       setWeb3(new Web3(window.ethereum));
     }
   }, []);
+  const contractAddresses = {
+    1440002: { // Xrp network
+      gameAddress: '0xa7169fDC282c990b510E2d6428bBC0ED1F7f1EfE',
+      nftAddress: '0x1319a2fff9189496b5420256a0b19bBD36aed6eB',
+    },
+    80002: { // Amoy network
+      gameAddress: '0x41Ba2b107DF51e61519b7b975a25bC4DF7eD5D06',
+      nftAddress: '0xf55754207c6d8Ed08fD0C5be3D00a4B581718A6a',
+    },
+    11155111: { // Sepolia network
+      gameAddress: '0x0f5841D53526aBdF15ef77c790aCd7839D86D9d0',
+      nftAddress: '0x1D5916eF2922d6783aF201A25af034d5024fA2f0',
+    },
+  };
 
   const switchChain = async () => {
     try {
@@ -46,12 +45,25 @@ export const Web3Provider = ({ children }) => {
         params: [{ chainId: `0x${(11155111).toString(16)}` }],
       });
     } catch (switchError) {
-      console.error('Switch Chain Error:', switchError);
+      if (switchError.code === 4902) {
+        console.log('This chain does not exist on the user wallet.');
+      } else {
+        console.error('Switch Chain Error:', switchError);
+      }
     }
   };
 
+  useEffect(() => {
+    if (nftContract) {
+      updateAchievements(walletAddress);
+    }
+  }, [nftContract, walletAddress]);
+
   const updateAchievements = async (walletAddress) => {
-    if (!nftContract) return;
+    if (!nftContract) {
+      console.error('nftContract is null');
+      return;
+    }
     const tempTokenIDs = [];
     for (let index = 1; index <= 17; index++) {
       try {
@@ -63,19 +75,15 @@ export const Web3Provider = ({ children }) => {
         console.error(`Failed to fetch balance for token ${index} on network ${Chain}. Error: ${e.message}`);
       }
     }
-    setTokenIDs(tempTokenIDs);
+    return setTokenIDs(tempTokenIDs);
   };
-
-  useEffect(() => {
-    if (nftContract) {
-      updateAchievements(walletAddress);
-    }
-  }, [nftContract, walletAddress]);
 
   useEffect(() => {
     const init = async () => {
       if (window.ethereum) {
-        const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+        const accounts = await window.ethereum.request({
+          method: 'eth_accounts',
+        });
         const chainId = parseInt(window.ethereum.chainId, 16);
         setChain(chainId);
         setWalletAddress(accounts[0] || '');
@@ -90,6 +98,8 @@ export const Web3Provider = ({ children }) => {
       if (addresses) {
         const gameContract = new web3.eth.Contract(gameABI, addresses.gameAddress);
         const nftContractInstance = new web3.eth.Contract(nftABI, addresses.nftAddress);
+        console.log('gameContract:', gameContract);
+        console.log('nftContractInstance:', nftContractInstance);
         setFactoryContract(gameContract);
         setNftContract(nftContractInstance);
       } else {
@@ -98,41 +108,93 @@ export const Web3Provider = ({ children }) => {
     }
   }, [Chain]);
 
+  useEffect(() => {
+    const init = async () => {
+      if (window.ethereum) {
+        const accounts = await window.ethereum.request({
+          method: 'eth_accounts',
+        });
+        const chainId = parseInt(window.ethereum.chainId, 16);
+        setChain(chainId);
+        setWalletAddress(accounts[0] || '');
+      }
+    };
+    init();
+  }, []);
+
+  useEffect(() => {
+    const handleAccountsChanged = async (accounts) => {
+      console.log(accounts);
+      console.log(accounts[0]);
+      setWalletAddress(accounts[0]);
+    };
+
+    const handleChainChanged = (chainId) => {
+      const decimalChainId = parseInt(chainId, 16);
+      setChain(decimalChainId);
+    };
+
+    if (window.ethereum) {
+      window.ethereum.on('accountsChanged', handleAccountsChanged);
+      window.ethereum.on('chainChanged', handleChainChanged);
+    }
+
+    return () => {
+      if (window.ethereum) {
+        window.ethereum.removeListener('accountsChanged', handleAccountsChanged);
+        window.ethereum.removeListener('chainChanged', handleChainChanged);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (walletAddress) {
+      localStorage.setItem('walletAddress', walletAddress);
+    }
+  }, [walletAddress]);
+
   const requestAccount = async () => {
     console.log('Requesting account...');
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
+    let provider;
+
     if (isMobile) {
-      // Open Web3Modal for mobile
-      const modal = new Web3Modal({ projectId: '9ea74fc0-0f80-4bc4-b98f-d6d4e256d34f' });
-      const provider = await modal.openModal();
+      // Use WalletConnect provider for mobile
+      provider = new WalletConnectProvider({
+        rpc: {
+          80002: 'https://rpc-mumbai.maticvigil.com', // Mumbai testnet RPC
+          1440002: 'https://rpc-for-chain-1440002', // Custom RPC for chain 1440002
+          11155111: 'https://zksync-sepolia.g.alchemy.com/v2/FdP4lSDr9rWv0rpT9qflxHA7KyBDoEDZ', // Sepolia testnet
+        },
+      });
 
-      const mobileWeb3 = new Web3(provider);
-      setWeb3(mobileWeb3);
-
-      try {
-        const accounts = await mobileWeb3.eth.getAccounts();
-        setWalletAddress(accounts[0]);
-        const chainId = await mobileWeb3.eth.getChainId();
-        setChain(chainId);
-      } catch (error) {
-        console.log('Error connecting account:', error);
-      }
+      // Enable WalletConnect provider
+      await provider.enable();
     } else if (window.ethereum) {
+      // Use MetaMask or any injected provider on desktop
+      provider = window.ethereum;
+
       const chainId = parseInt(window.ethereum.chainId, 16);
       setChain(chainId);
+      console.log(chainId);
+
+      // Switch chain if not on a desired network
       if (chainId !== 80002 && chainId !== 1440002 && chainId !== 11155111) {
         await switchChain();
       }
-
-      try {
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-        setWalletAddress(accounts[0]);
-      } catch (error) {
-        console.log(error);
-      }
     } else {
       console.log('No Ethereum provider found');
+      return;
+    }
+
+    const web3 = new Web3(provider);
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      setWalletAddress(accounts[0]);
+    } catch (error) {
+      console.log('Error connecting account:', error);
     }
   };
 
